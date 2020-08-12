2be3facc484bd6c6a82a10cba7fd7b99eb3a87b2
mattdavis0351 <mattdavis0351@github.com>
2020-08-12 14:39:20 -0700

    more

--- index.js
+++ index.js
@@ -3,32 +3,29 @@
  * @param {import('probot').Application} app
  */
 
-// const createEntitlement = require("./lib/createEntitlement");
-const sendUserInvitation = require("./lib/sendUserInvitation");
-const commentWithStatus = require("./lib/commentWithStatus");
-
-// const fs = require("fs");
+const sendUserInvitation = require('./lib/sendUserInvitation')
+const commentWithStatus = require('./lib/commentWithStatus')
 
 module.exports = (app) => {
-  // Your code here
-  app.log("Yay, the app was loaded!");
+  app.log('Yay, the app was loaded!')
 
-  app.on("issue_comment.created", async (context) => {
-    const bot = context.isBot;
-    const { body } = context.payload.comment;
-    const containsCommand = body.includes("/add-me");
-
+  app.on('issue_comment.created', async (context) => {
+    const bot = context.isBot
+    const { body } = context.payload.comment
+    const containsCommand = body.includes('/add-me')
     try {
-      app.log("you are inside issue_comment.created");
       if (!bot && containsCommand) {
-        console.log("from index... trying to invoke sendUserInvitation");
-        const inviteResp = await sendUserInvitation(context);
-        console.log("from index... trying to invoke commentWithStatus");
-        const commentResp = await commentWithStatus(context);
-        return { inviteResp, commentResp };
+        await sendUserInvitation(context)
+
+        await commentWithStatus(context)
+
+        // return { inviteResp, commentResp }
+      }
+      if (bot) {
+        return false
       }
     } catch (error) {
-      app.log(error);
+      app.log(error.message)
     }
-  });
-};
+  })
+}
